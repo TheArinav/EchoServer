ecb920569867d4e7e82f82d4804d47ad234de0ac
TheArinav <88740085+TheArinav@users.noreply.github.com>
2024-07-03 20:10:30 +0300

    Program Working

---
+++ CodeFiles/client.h
@@ -0,0 +1,6 @@
+#ifndef PROJ1_CLIENT_H
+#define PROJ1_CLIENT_H
+
+void StartClient();
+
+#endif //PROJ1_CLIENT_H
---
+++ CodeFiles/server.h
@@ -0,0 +1,6 @@
+#ifndef PROJ1_SERVER_H
+#define PROJ1_SERVER_H
+
+void StartServer();
+
+#endif //PROJ1_SERVER_H
